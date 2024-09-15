d5ec7b8a8bf770278f25c4bf8b112b492455ac64
mailsanjayhere <33064356+mailsanjayhere@users.noreply.github.com>
2024-09-15 06:30:00 +0530

    Update robo_car_online_program.cpp
    modified digital to analog

--- robo_car_online_program.cpp
+++ robo_car_online_program.cpp
@@ -2,40 +2,29 @@
 #include <Servo.h>
 #include <AFMotor.h>
 
-//hc-sr04 sensor
+// Sensors and motors setup
 #define TRIGGER_PIN A2
 #define ECHO_PIN A3
 #define max_distance 50
-
-//ir sensor
 #define irLeft A0
 #define irRight A1
 
-//motor
-#define MAX_SPEED 200
-#define MAX_SPEED_OFFSET 20
-
 Servo servo;
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, max_distance);
-
 AF_DCMotor motor1(1, MOTOR12_1KHZ);
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
 AF_DCMotor motor3(3, MOTOR34_1KHZ);
 AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
-
-int distance = 0;
-int leftDistance;
-int rightDistance;
-boolean object;
+int distance = 0, leftDistance, rightDistance, leftIR, rightIR;
+int irThreshold = 500;  // Adjust based on your environment
 
 void setup() {
   Serial.begin(9600);
   pinMode(irLeft, INPUT);
   pinMode(irRight, INPUT);
-  servo.attach(10);
-  servo.write(90);
+  servo.attach(9);
+  servo.write(90);  // Center servo
 
   motor1.setSpeed(120);
   motor2.setSpeed(120);
@@ -44,54 +33,49 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(irLeft) == 0 && digitalRead(irRight) == 0 ) {
-    objectAvoid();
-    //forword
-  }
-  else if (digitalRead(irLeft) == 0 && digitalRead(irRight) == 1 ) {
+  // Read IR sensor values as analog
+  leftIR = analogRead(irLeft);
+  rightIR = analogRead(irRight);
+
+  Serial.print("Left IR: ");
+  Serial.print(leftIR);
+  Serial.print(" - Right IR: ");
+  Serial.println(rightIR);
+
+  // Adjust logic based on analog values instead of digital
+  if (leftIR > irThreshold && rightIR > irThreshold) {
+    objectAvoid();  // Both sensors detect the line
+  } else if (leftIR > irThreshold && rightIR <= irThreshold) {
     objectAvoid();
     Serial.println("TL");
-    //leftturn
-    moveLeft();
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 0 ) {
+    moveLeft();  // Left detects black, turn left
+  } else if (leftIR <= irThreshold && rightIR > irThreshold) {
     objectAvoid();
     Serial.println("TR");
-    //rightturn
-    moveRight();
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 1 ) {
-    //Stop
-    Stop();
+    moveRight();  // Right detects black, turn right
+  } else {
+    Stop();  // Both sensors off-track
   }
 }
 
 void objectAvoid() {
   distance = getDistance();
   if (distance <= 15) {
-    //stop
     Stop();
-    Serial.println("Stop");
-
+    Serial.println("Obstacle detected! Stopping...");
     lookLeft();
     lookRight();
     delay(100);
     if (rightDistance <= leftDistance) {
-      //left
       object = true;
       turn();
       Serial.println("moveLeft");
     } else {
-      //right
       object = false;
       turn();
       Serial.println("moveRight");
     }
-    delay(100);
-  }
-  else {
-    //forword
-    Serial.println("moveforword");
+  } else {
     moveForward();
   }
 }
@@ -105,51 +89,43 @@ int getDistance() {
   return cm;
 }
 
-int lookLeft () {
-  //lock left
+int lookLeft() {
   servo.write(150);
   delay(500);
   leftDistance = getDistance();
-  delay(100);
   servo.write(90);
-  Serial.print("Left:");
-  Serial.print(leftDistance);
   return leftDistance;
-  delay(100);
 }
 
 int lookRight() {
-  //lock right
   servo.write(30);
   delay(500);
   rightDistance = getDistance();
-  delay(100);
   servo.write(90);
-  Serial.print("   ");
-  Serial.print("Right:");
-  Serial.println(rightDistance);
   return rightDistance;
-  delay(100);
 }
+
 void Stop() {
   motor1.run(RELEASE);
   motor2.run(RELEASE);
   motor3.run(RELEASE);
   motor4.run(RELEASE);
 }
+
 void moveForward() {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
   motor3.run(FORWARD);
   motor4.run(FORWARD);
 }
+
 void moveBackward() {
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
   motor3.run(BACKWARD);
   motor4.run(BACKWARD);
-
 }
+
 void turn() {
   if (object == false) {
     Serial.println("turn Right");
@@ -159,13 +135,7 @@ void turn() {
     delay(800);
     moveRight();
     delay(900);
-    if (digitalRead(irRight) == 1) {
-      loop();
-    } else {
-      moveForward();
-    }
-  }
-  else {
+  } else {
     Serial.println("turn left");
     moveRight();
     delay(700);
@@ -173,19 +143,16 @@ void turn() {
     delay(800);
     moveLeft();
     delay(900);
-    if (digitalRead(irLeft) == 1) {
-      loop();
-    } else {
-      moveForward();
-    }
   }
 }
+
 void moveRight() {
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
   motor3.run(FORWARD);
   motor4.run(FORWARD);
 }
+
 void moveLeft() {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
